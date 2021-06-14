64bf7b0182b8dca837769bbe0eeb7abc0129a480
DarioRugg <56448497+DarioneNazionale@users.noreply.github.com>
2021-06-14 15:30:21 +0200

    adding the utils.py

---
+++ working_on_data/utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import json
+
+def get_categories(entry):
+    if "category" not in entry.keys(): return pd.Series()
+
+    category_dict = json.loads(entry["category"])
+    if "parent_name" in category_dict.keys():
+        return pd.Series({
+            "category": category_dict["parent_name"],
+            "sub_category": category_dict["name"]
+        })
+    else:
+        return pd.Series({
+            "category": category_dict["name"]
+        })
+
+
+def get_urls(entry):
+    return pd.Series({"project_url": json.loads(entry["urls"])["web"]["project"]})
+
+
+def get_creator(entry):
+    return pd.Series({"creator_id": int(re.search(r"(?<=\"id\":)\d+(?=,)", entry["creator"]).group(0))})
