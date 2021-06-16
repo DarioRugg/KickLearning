03577caf3a689cd532315db2f96cd51ec2500e71
DarioRugg <56448497+DarioneNazionale@users.noreply.github.com>
2021-06-16 17:38:05 +0200

    realized the tfidf model

---
+++ feature_analysis/text_embeding.py
@@ -0,0 +1,90 @@
+import unicodedata
+
+from sklearn.exceptions import NotFittedError
+
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import KernelPCA
+
+nltk.download('stopwords', quiet=True)
+
+
+class TextEncoder:
+    def __init__(self, kpca_n_components=7, data=None):
+
+        self.data = data
+
+        self.stop_words = set(stopwords.words('english'))
+        self.tokenizer = RegexpTokenizer(r'\w+')
+        self.stammer = SnowballStemmer(language="english")
+
+        self.tfidf_transformer = TfidfVectorizer(stop_words=stopwords.words('english'))
+
+        self.pca_transformer = KernelPCA(n_components=kpca_n_components, kernel="rbf")
+
+    def _preprocessing(self, text):
+        line = text.lower()
+
+        word = line.split()
+        # removing stop words
+        words = filter(lambda w: w not in self.stop_words, word)
+        # removing punctuation from each word
+        punct_remove = list(map(lambda w: " ".join(self.tokenizer.tokenize(w)), words))
+        # removing stem
+        stemmed = list(map(self.stammer.stem, punct_remove))
+        # removing Accent
+        accents = list(
+            map(lambda w: unicodedata.normalize(u'NFKD', w).encode('ascii', 'ignore').decode('utf8'), stemmed))
+        line = " ".join(accents)
+
+        return line.strip()
+
+    def new_data(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def preprocess(self):
+        self.data = list(map(self._preprocessing, self.data))
+
+    def tfidf_fit(self, data=None):
+        self.tfidf_transformer.fit(data if data is not None else self.data)
+
+    def pca_fit(self, data=None):
+        self.pca_transformer.fit(data if data is not None else self.data)
+
+    def tfidf_transform(self):
+        try:
+            self.data = self.tfidf_transformer.transform(self.data)
+        except NotFittedError as e:
+            print("You must first fit on the data")
+            raise e
+
+    def pca_transform(self):
+        try:
+            self.data = self.pca_transformer.transform(self.data)
+        except NotFittedError as e:
+            print("You must first fit on the data")
+            raise e
+
+    def complete_pipeline(self, fit_on_data=False):
+        self.preprocess()
+
+        try:
+
+            if fit_on_data: self.tfidf_fit()
+            self.tfidf_transform()
+
+            if fit_on_data: self.pca_fit()
+            self.pca_transform()
+
+        except NotFittedError as e:
+            print("You can also set 'fit_on_data=True'")
+            raise e
+
+        return self.get_data()
